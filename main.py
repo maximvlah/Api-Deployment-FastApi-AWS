b18d1e1e5bbe432f4da422af50ddb2774613231c
Maxim Vlah <maxim@ourz.world>
2022-06-27 19:55:05 +0200

    working messy demo

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ import os
 import logging
 import sys
 
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+import yaml
+import boto3
+import base64
+import pickle
+
+import os
+import json
+import base64
+from io import BytesIO
+import requests
+
+from annoy import AnnoyIndex
+
+from tqdm import tqdm
+
+import tensorflow as tf
+import tensorflow_hub as hub
+# from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.applications.efficientnet import preprocess_input
 
 from urllib.parse import urlparse
 
@@ -58,14 +76,7 @@ class ImageOutput(BaseModel):
     """
     Represents the result of a prediction
     """
-    score: Optional[float] = 0.0
-    category: Optional[str] = None
-    name: Optional[str] = None
-
-    @validator('score')
-    def result_check(cls, v):
-        return round(v, 4)
-
+    embedding: List[float] = None
 
 class PredictRequest(BaseModel):
     """
@@ -73,19 +84,148 @@ class PredictRequest(BaseModel):
     """
     images: List[ImageInput] = []
 
-
 class PredictResponse(BaseModel):
     """
     Represents a request to process
     """
-    images: List[ImageOutput] = []
+    embeddings: List[ImageOutput] = []
 
-
-app = FastAPI()
-
+class ProductSearchResponse(BaseModel):
+    """
+    Represents a request to process
+    """
+    best_matches: dict = None
 
 class ImageNotDownloadedException(Exception):
     pass
+
+def download_files_from_s3(bucket_name,
+                           s3_client,
+                           files):
+    """Downloads files from S3 bucket
+    Args:
+        bucket_name (str): name of S3 bucket
+        s3_client (boto3.client): S3 client
+        files (list[str]): list of files to download
+    """
+    # Get list of files in S3 bucket
+    for f in tqdm(files, desc="Downloading files from S3", total=len(files)):
+
+        if not os.path.exists(f):
+            s3_client.download_file(bucket_name, f, f)
+        else:
+            print(f"{f} already exists!")
+    
+def retireve_data_from_aws(idxs,s3_client,table,partition_key="code"):
+    """Retrieves data for product from AWS
+    Args:
+        idxs (list[int]): best match idxs
+        s3_client (boto3.client): S3 client
+        table (boto3.dynamodb.Table): DynamoDB table
+    
+    Returns:
+        data (dict): data for best matches
+    """
+
+    search_results = {}
+    for idx in range(len(idxs)):
+
+        # Get data from DynamoDB
+        ean = IDX_TO_EAN_MAP[idxs[idx]]
+        response = table.get_item(
+            Key={
+                partition_key: str(ean) #NOTE: converting to string to match DynamoDB partition key type
+            }
+        )
+        best_matches_metadata = response['Item']
+
+        # Load image from S3 bucket and convert to Base64
+        #NOTE: using crops of original images instead of original images
+        s3_image = s3_client.get_object(Bucket=CONFIG["bucket_name"], Key=f"crops/{ean}_.jpg") 
+        s3_image = Image.open(io.BytesIO(s3_image['Body'].read())).convert('RGB')
+        best_matches_metadata['image_b64'] = img2b64(s3_image)
+
+        # Append to search_results
+        search_results[str(idx)] = best_matches_metadata
+
+    return search_results
+
+def load_annoy(path,
+               num_dimensions=2048,
+               metric="angular"):
+    """
+    Loads Annoy Index from disk
+
+    Args:
+        path (str): path to Annoy Index
+        num_dimensions (int): number of dimensions of the embedding vector
+        metric (str): metric to use for Annoy Index
+
+    Returns:
+        annoy_index (AnnoyIndex): Annoy Index
+
+    """
+
+    annoy_index = AnnoyIndex(num_dimensions,metric)
+    annoy_index.load(path)
+
+    print(f"Loaded Annoy Index from {path}")
+    return annoy_index
+
+def img2b64(image):
+    """Converts PIL image to Base64 encoded string
+    Args:
+        image (PIL.image)
+    Returns:
+        b64 (str): Base64 encoded string
+    """
+
+    buff = BytesIO()
+    image.save(buff, format="JPEG")
+    img_str = base64.b64encode(buff.getvalue())
+    return img_str.decode("utf-8")
+
+def b642bytes(b64_string):
+    """
+    Converts b64 string to bytes
+    :param b64_string: string to convert
+    :return: bytes
+    """
+    bytes = str.encode(b64_string)
+    bytes = base64.b64decode(bytes)
+    return bytes
+
+with open("serve_config.yaml", "r") as stream:
+    try:
+        CONFIG = (yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
+        CONFIG = None
+assert CONFIG is not None, "Could not load config file."
+
+ # FastAPI app
+app = FastAPI()
+
+# Connect to DynamoDB
+dynamodb = boto3.resource('dynamodb',region_name=CONFIG["region_name"])
+table = dynamodb.Table(CONFIG["table_name"])
+
+# Connect to S3
+s3_client = boto3.client('s3',region_name=CONFIG["region_name"])
+
+# Download files needed for vector search from S3
+# TODO: What if the search index will be very large?
+files = ["search_index.ann","idx_to_ean_map.pickle"]
+download_files_from_s3(CONFIG["bucket_name"],
+                       s3_client,
+                       files)
+
+# Load mapping from idx to EAN.
+with open("idx_to_ean_map.pickle", "rb") as f: 
+    IDX_TO_EAN_MAP = pickle.load(f)
+
+# Load Annoy Search Index
+annoy = load_annoy("search_index.ann", CONFIG["embedding_size"], CONFIG["metric"])
 
 
 @app.exception_handler(Exception)
@@ -112,7 +252,7 @@ def load_model():
     if not hasattr(app.state, 'model'):
         configure_logging()
         logger.info('Loading models...')
-        app.state.model = ImageClassifier()
+        app.state.model = ImageEmbedder()
 
 
 def configure_logging(logging_level=logging.INFO):
@@ -128,19 +268,83 @@ def configure_logging(logging_level=logging.INFO):
     root.addHandler(stream_handler)
 
 
-class ImageClassifier:
+# class ImageClassifier:
+#     """
+#     Classifies images according to ImageNet categories.
+#     """
+#     def __init__(self):
+#         """
+#         Prepares the model used by the application for use.
+#         """
+#         self.model = MobileNetV2()
+#         _, height, width, channels = self.model.input_shape
+#         self.input_width = width
+#         self.input_height = height
+#         self.input_channels = channels
+
+#     def _prepare_images(self, images):
+#         """
+#         Prepares the images for prediction.
+
+#         :param images: The list of images to prepare for prediction in Pillow Image format.
+
+#         :return: A list of processed images.
+#         """
+#         batch = np.zeros((len(images), self.input_height, self.input_width, self.input_channels), dtype=np.float32)
+#         for i, image in enumerate(images):
+#             x = image.resize((self.input_width, self.input_height), Image.BILINEAR)
+#             batch[i, :] = np.array(x, dtype=np.float32)
+#         batch = preprocess_input(batch)
+#         return batch
+
+#     def predict(self, images, batch_size):
+#         """
+#         Predicts the category of each image.
+
+#         :param images: A list of images to classify.
+#         :param batch_size: The number of images to process at once.
+
+#         :return: A list containing the predicted category and confidence score for each image.
+#         """
+#         batch = self._prepare_images(images)
+#         scores = self.model.predict(batch, batch_size)
+#         results = decode_predictions(scores, top=1)
+#         return results
+
+class ImageEmbedder:
     """
-    Classifies images according to ImageNet categories.
+    Calculates image embedding.
     """
     def __init__(self):
         """
         Prepares the model used by the application for use.
         """
-        self.model = MobileNetV2()
-        _, height, width, channels = self.model.input_shape
+        # self.model = tf.keras.models.load_model("/home/maxim/projects/ourz/Api-Deployment-FastApi-AWS/models/embedder")
+        m = tf.keras.Sequential([hub.KerasLayer("https://tfhub.dev/tensorflow/resnet_50/feature_vector/1",trainable=False),
+        ])
+        m.build([None, 224, 224, 3])
+
+        self.model = m
+
+        _, height, width, channels = None,224,224,3
         self.input_width = width
         self.input_height = height
         self.input_channels = channels
+
+    def _preprocess_img(self,bytes):
+        """
+        Processes image bytes in format suitable for tf inference
+
+        :param bytes (bytes)
+        :return img (np.array): preprocessed image
+        """
+        img = tf.io.decode_image(bytes,channels=self.input_channels)
+
+        img = tf.image.resize(img, 
+                              method="bilinear", 
+                              size=(self.input_width,self.input_height))
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        return img
 
     def _prepare_images(self, images):
         """
@@ -151,9 +355,9 @@ class ImageClassifier:
         :return: A list of processed images.
         """
         batch = np.zeros((len(images), self.input_height, self.input_width, self.input_channels), dtype=np.float32)
-        for i, image in enumerate(images):
-            x = image.resize((self.input_width, self.input_height), Image.BILINEAR)
-            batch[i, :] = np.array(x, dtype=np.float32)
+        for i, image_bytes in enumerate(images):
+            # x = image.resize((self.input_width, self.input_height), Image.BILINEAR)
+            batch[i, :] = np.array(self._preprocess_img(image_bytes), dtype=np.float32)
         batch = preprocess_input(batch)
         return batch
 
@@ -167,10 +371,8 @@ class ImageClassifier:
         :return: A list containing the predicted category and confidence score for each image.
         """
         batch = self._prepare_images(images)
-        scores = self.model.predict(batch, batch_size)
-        results = decode_predictions(scores, top=1)
-        return results
-
+        embeddings = self.model.predict(batch, batch_size)
+        return embeddings
 
 def get_url_scheme(url, default_scheme='unknown'):
     """
@@ -186,7 +388,8 @@ async def retrieve_content(entry, sess, sem):
     """
     raw_data = None
     if entry.data is not None:
-        raw_data = base64.b64decode(entry.data)
+        # raw_data = base64.b64decode(entry.data)
+        raw_data = b642bytes(entry.data)
     elif entry.url is not None:
         source_uri = entry.url
         scheme = get_url_scheme(source_uri)
@@ -195,9 +398,10 @@ async def retrieve_content(entry, sess, sem):
         else:
             raise ValueError('Invalid scheme: %s' % scheme)
     if raw_data is not None:
-        image = Image.open(io.BytesIO(raw_data))
-        image = image.convert('RGB')
-        return image
+        # image = Image.open(io.BytesIO(raw_data))
+        # image = image.convert('RGB')
+        # return image
+        return raw_data
     return None
 
 
@@ -250,31 +454,34 @@ def predict_images(images):
     :return: The prediction results.
     """
     response = list()
-    results = app.state.model.predict(images, BATCH_SIZE)
-    for top_n in results:
-        category, name, score = top_n[0]
-        response.append(ImageOutput(category=category, name=name, score=score))
-    return response
+    results = app.state.model.predict(images, BATCH_SIZE)[0]
+    print(results.shape)
 
+    # response.append(ImageOutput(embedding=results.tolist()))
+    return results
 
-@app.post('/v1/predict', response_model=PredictResponse)
+@app.post('/v1/predict', response_model=ProductSearchResponse)
 async def process(req: PredictRequest):
     """
     Predicts the category of the images contained in the request.
 
     :param req: The request object containing the image data to predict.
 
-    :return: The prediction results.
+    :return: Search results.
     """
-    logger.info('Processing request.')
+    logger.info('Processing request...')
     logger.debug(req.json())
-    logger.info('Downloading images.')
+    logger.info('Downloading images...')
     images = await retrieve_images(req.images)
-    logger.info('Performing prediction.')
-    results = predict_images(images)
+    logger.info('Performing prediction...')
+    predictions = predict_images(images)
+    logger.info('Searching...')
+    best_matches_idxs = annoy.get_nns_by_vector(predictions,CONFIG["k"])
+    logger.info('Retrieving data from AWS...')
+    search_results = retireve_data_from_aws(best_matches_idxs,s3_client,table)
     logger.info('Transaction complete.')
-    return PredictResponse(images=results)
 
+    return ProductSearchResponse(best_matches=search_results)
 
 @app.get('/health')
 def test():
